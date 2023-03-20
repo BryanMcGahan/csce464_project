ee1ade138fe0d4fafc6f7eae039451e851e87630
BryanMcGahan <mcgahan72@outlook.com>
2023-03-20 13:57:06 -0500

    Had to restructure git repo

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,5 +1,25 @@
+let sessionStorage = window.sessionStorage;
+let currentMode = sessionStorage.getItem("dark-mode");
+var body = document.body;
+
+console.log(currentMode);
+if (currentMode === "false") {
+	currentMode = "light";
+}
+
+if(currentMode === "dark"){
+	body.classList.toggle('dark-mode');
+}
+
+
 
 function toggle_darkmode() {
-	var body = document.body;
 	body.classList.toggle('dark-mode')
+	if (currentMode === "light") {
+		currentMode = "dark"
+	} else if (currentMode === "dark") {
+		currentMode = "light"
+	}
+	console.log(currentMode);
+	sessionStorage.setItem('dark-mode', currentMode);
 }
